eb3b13e793d4e4b088f6113db339b6986de8388e
Marco Jaram <marco.jaramillocast@gmail.com>
2021-08-21 00:26:29 -0700

    se agrega condicinal multiple  if para evitar introducir numero menor a 0 y mayor a 100

--- src/main/java/CaulculoPromedio.java
+++ src/main/java/CaulculoPromedio.java
@@ -1,4 +1,5 @@
 //Este programa saca el promedio de la calificacion de 10 estudiantes
+
 import java.util.Scanner;
 
 public class CaulculoPromedio {
@@ -7,18 +8,18 @@ public class CaulculoPromedio {
         int estudiante = 1;
         int calificacion;
         double suma = 0;
-//        se inicializa en cero porque se ira sumando con cada entrada de calificacion
         Scanner input = new Scanner(System.in);
-//       input es el objeto para poder almacenar las variables que entran por el teclado
         while (estudiante <= 10) {
             System.out.println("Ingresa la calificacion del estudiante: " + estudiante);
             calificacion = input.nextInt();
-            estudiante++;
-            suma = suma +calificacion;
-//            suma+= calificacion;
-//    +=	Suma y asignación	a += b (a=a + b)
+            if (calificacion <= 100 && calificacion >= 0) {
+                estudiante++;
+                suma = suma + calificacion;
+            } else {
+                System.out.println("Ingresa la calificacion del estudiante en un rango del 0 - 100: ");
+            }
         }
-        System.out.println("El promedio de la calificiacion es "+ (suma/100));
+        System.out.println("El promedio de la calificiacion es " + (suma / 100));
     }
 
 }
